d52da6ac68122fd17fd44fa4b3876060673b9e88
Svavar Pall Gudgeirsson <spg009@gmail.com>
2020-11-12 10:20:06 +0000

    got the image source

--- webscrape.py
+++ webscrape.py
@@ -10,7 +10,7 @@ driver = webdriver.Chrome(chrome_loc)
 
 # you can use different browsers, I just like chrome
 # then we get the webpage.
-driver.get("somesite")
+driver.get("https://www.artstation.com/?sort_by=trending&dimension=2d")
 
 # then we get the content form this page
 content = driver.page_source
@@ -18,3 +18,18 @@ content = driver.page_source
 # html.parser tells the Beautiful soup that it's looking
 # at a website, and will act acordingly
 soup = BeautifulSoup(content, 'html.parser')
+
+# where we will then store the images
+image = []
+# then we will loop through the soup and see if we find what we are looking for
+for a in soup.find_all('div', attrs={'class': 'gallery-grid'}):
+    images = a.find_all('img', attrs={'class': 'd-block'})
+    for item in images:
+        image.append(item['src'])
+
+# then we loop through and print out the item in the array
+for item in image:
+    print(item)
+
+# and then we close the selenium, or it's driver
+driver.close()
